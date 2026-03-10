cdaa9a165df7b75d50e75395a7bee7a4609d29fe
Melvor-PC <ruslanseitbekirov6632@gmail.com>
2026-03-10 22:50:53 +0300

    lab1 Gorska

--- cpp2/Algoritm/lab1.cpp
+++ cpp2/Algoritm/lab1.cpp
@@ -25,14 +25,32 @@ double simpson_even(double a, double h, int n) {
     return 2 * total;
 }
 
+double simpson_adaptive(double f(double), double a, double b, double eps, int max_n, double h) {
+    int n = 4;                     // начальное число отрезков
+    double I_h = (h / 3.0) * (f(a) + simpson_odd(a, h, n) + simpson_even(a, h, n) + f(b));
+    while (n <= max_n) {
+        n *= 2;
+        double I_h2 = (h / 3.0) * (f(a) + simpson_odd(a, h, n) + simpson_even(a, h, n) + f(b));
+        double err = fabs(I_h2 - I_h) / 15.0;   // оценка погрешности по Рунге
+        if (err < eps) {
+            // уточнённое значение (экстраполяция)
+            return I_h2 + (I_h2 - I_h) / 15.0;
+        }
+        I_h = I_h2;
+    }
+    // если не сошлись — обработка ошибки
+    return I_h; // или throw
+}
+
+
 int main() {
-    double a, b;
+    double a, b, eps = 1.0/10000;
     int n;
     cout << "Введите начало интервала: ";
     cin >> a;
     cout << "Введите конец интервала(через точку): ";
     cin >> b;
-    cout << "Введите количество разбиений(n - должно быть чётным): ";
+    cout << "Введите максимальное количество разбиений(n - должно быть чётным): ";
     cin >> n;          // число отрезков (должно быть чётным)
     if (n % 2 != 0){
         return 0;
@@ -41,5 +59,6 @@ int main() {
     // Вычисляем интеграл по составной формуле Симпсона
     double I = (h / 3.0) * (f(a) + simpson_odd(a, h, n) + simpson_even(a, h, n) + f(b));
     cout << "Результат: " << I << endl;
+    cout << "Результат: " << simpson_adaptive(f, a, b, eps, n, h) << endl;
     return 0;
-}
+} 
