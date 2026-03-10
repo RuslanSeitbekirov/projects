277fbbd699d20ac92c6ef567c6b85e672a69a52d
Ruslan Seitbekirov <ruslanseitbekirov6632@gmail.com>
2026-03-10 20:53:13 +0300

    2

--- lab2/cli/cpp/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main(int argc, char** argv){
-    for(int i=0; i<argc; i++)
-        std::cout << i << ": " << argv[i] << std::endl;
-}
--- lab2/cli/go.mod
+++
@@ -1,5 +0,0 @@
-module cli
-
-go 1.25.3
-
-require github.com/joho/godotenv v1.5.1
