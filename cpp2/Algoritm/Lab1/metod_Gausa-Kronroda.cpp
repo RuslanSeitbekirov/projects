c68cf4102678c10e3b2b5006678aa571ac2b7b87
Ruslan Seitbekirov <ruslanseitbekirov6632@gmail.com>
2026-03-13 17:13:25 +0300

    1

---
+++ cpp2/Algoritm/Lab1/metod_Gausa-Kronroda.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<double> dot = [];
+
+    double a, b, eps;
+    cout << "Введите начало интервала: ";
+    cin >> a;
+    cout << "Введите конец интервала(через точку): ";
+    cin >> b;
+    cout << "Введите желаемую точность: ";
+    cin >> eps;
+    double result;
+
+    cout << "Результат: " << result << endl;
+    return 0;
+}
