3f312473f727499de6abfd40ab40a942f6f13b27
Ruslan Seitbekirov <ruslanseitbekirov6632@gmail.com>
2026-03-13 11:04:43 +0300

    13.03.2026 simpson update

---
+++ cpp2/Algoritm/Lab1/adapt_simpson.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+double f(double x) {
+    return 1.0 / ((0.5 * sin(x) + 3 * cos(x)) * (0.5 * sin(x) + 3 * cos(x)));
+}
+
+
+double rule(double f(double), double a, double b){
+    double h = (b - a)/2;
+    return (h/3) * (f(a) + 4*f((a+b)/2) + f(b));
+}
+
+double simpson_adapt(double f(double), double a, double b, double eps, int max_depth=50){
+    double m = (a+b)/2;
+    double whole = rule(f, a, b);
+    double left = rule(f, a, m);
+    double right = rule(f, m, b);
+    // Подсчёт погрешности по правилу Рунге
+    auto delta = abs(left + right - whole) / 15;
+
+    if (delta < eps * (b-a)/(b-a) || max_depth == 0){
+        return left + right;
+    } else{
+        return (simpson_adapt(f, a, m, eps / 2, max_depth - 1) + simpson_adapt(f, m, b, eps / 2, max_depth - 1));
+    }
+    
+}
+
+int main() {
+    double a, b, eps;
+    cout << "Введите начало интервала: ";
+    cin >> a;
+    cout << "Введите конец интервала(через точку): ";
+    cin >> b;
+    cout << "Введите желаемую точность: ";
+    cin >> eps;
+    double result = simpson_adapt(f, a, b, eps);
+
+    cout << "Результат: " << result << endl;
+    return 0;
+} 
