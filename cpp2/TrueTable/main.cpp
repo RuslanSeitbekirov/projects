95da8903a6d6635117a4092f745aacc73b7be334
Melvor-PC <ruslanseitbekirov6632@gmail.com>
2026-03-17 22:10:39 +0300

    3

--- cpp2/TrueTable/main.cpp
+++ cpp2/TrueTable/main.cpp
@@ -121,65 +121,65 @@
 // }
 
 // Ф3
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-// Антиимпликация: x и не y (x ↛ y)
-bool antiImpl(bool x, bool y) {
-    return x && !y;
-}
+// // Антиимпликация: x и не y (x ↛ y)
+// bool antiImpl(bool x, bool y) {
+//     return x && !y;
+// }
 
-// Антирепликация: не x и y (обратная антиимпликация, y ↛ x)
-bool antiRepl(bool x, bool y) {
-    return !x && y;
-}
+// // Антирепликация: не x и y (обратная антиимпликация, y ↛ x)
+// bool antiRepl(bool x, bool y) {
+//     return !x && y;
+// }
 
-// Штрих Шефера | (NAND): не (x и y)
-bool nand(bool x, bool y) {
-    return !(x && y);
-}
+// // Штрих Шефера | (NAND): не (x и y)
+// bool nand(bool x, bool y) {
+//     return !(x && y);
+// }
 
-// Стрелка Пирса ↓ (NOR): не (x или y)
-bool nor(bool x, bool y) {
-    return !(x || y);
-}
+// // Стрелка Пирса ↓ (NOR): не (x или y)
+// bool nor(bool x, bool y) {
+//     return !(x || y);
+// }
 
-int over(int a, int b) {
-    if ((a + b) < 2)  {
-        return a + b;
-    } else {
-        return 0;
-    }
-}
+// int over(int a, int b) {
+//     if ((a + b) < 2)  {
+//         return a + b;
+//     } else {
+//         return 0;
+//     }
+// }
 
-void print(int a, int b){
-    a = 3;
-    cout << "| x | y | z | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | " << endl;
-    cout << "|:-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|" << endl;
-    for (int x = 0; x < 2; ++x) {
-        for (int y = 0; y < 2; ++y) {
-            for (int z = 0; z < 2; ++z) {
-                cout << "| " << x << " | " << y << " | " << z << " | "
-                    << (z || y) << " | " // 1
-                    << ((z || y) == x) << " | " // 2
-                    << !((z || y) == x) << " | " // 3                                 
-                    << antiImpl(!((z || y) == x), y) << " | " // 4
-                    << !(y) << " | " // 5
-                    << (z && x) << " | " // 6
-                    << over(!(y), (z && x)) << " | " // 7
-                    << nand(antiImpl(!((z || y) == x), y), over(!(y), (z && x))) << " | " << endl; // 8
-            }
-        }
-    }
+// void print(int a, int b){
+//     a = 3;
+//     cout << "| x | y | z | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | " << endl;
+//     cout << "|:-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|" << endl;
+//     for (int x = 0; x < 2; ++x) {
+//         for (int y = 0; y < 2; ++y) {
+//             for (int z = 0; z < 2; ++z) {
+//                 cout << "| " << x << " | " << y << " | " << z << " | "
+//                     << (z || y) << " | " // 1
+//                     << ((z || y) == x) << " | " // 2
+//                     << !((z || y) == x) << " | " // 3                                 
+//                     << antiImpl(!((z || y) == x), y) << " | " // 4
+//                     << !(y) << " | " // 5
+//                     << (z && x) << " | " // 6
+//                     << over(!(y), (z && x)) << " | " // 7
+//                     << nand(antiImpl(!((z || y) == x), y), over(!(y), (z && x))) << " | " << endl; // 8
+//             }
+//         }
+//     }
 
-}
+// }
 
-int main()
-{
-    int a = 1;
-    int b = 1;
-    print(a, b);
-}
+// int main()
+// {
+//     int a = 1;
+//     int b = 1;
+//     print(a, b);
+// }
 
 
 // //Ф4
@@ -369,3 +369,237 @@ int main()
 //     int b = 1;
 //     print(a, b);
 // }
+
+//Лабораторная 2
+//№1
+// #include <iostream>
+// using namespace std;
+
+// // Антиимпликация: x и не y (x ↛ y)
+// bool antiImpl(bool x, bool y) {
+//     return x && !y;
+// }
+
+// // Антирепликация: не x и y (обратная антиимпликация, y ↛ x)
+// bool antiRepl(bool x, bool y) {
+//     return !x && y;
+// }
+
+// // Штрих Шефера | (NAND): не (x и y)
+// bool nand(bool x, bool y) {
+//     return !(x && y);
+// }
+
+// // Стрелка Пирса ↓ (NOR): не (x или y)
+// bool nor(bool x, bool y) {
+//     return !(x || y);
+// }
+
+// int over(int a, int b) {
+//     if ((a + b) < 2)  {
+//         return a + b;
+//     } else {
+//         return 0;
+//     }
+// }
+
+// void print(int a, int b){
+//     a = 3;
+//     cout << "| x1 | x2 | x3 | 1 | 2 | 3 | 4 | 5 |" << endl;
+//     cout << "|:-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|" << endl;
+//     for (int x = 0; x < 2; ++x) {
+//         for (int y = 0; y < 2; ++y) {
+//             for (int z = 0; z < 2; ++z) {
+//                 cout << "| " << x << " | " << y << " | " << z << " | "
+//                     << over(z, !(y)) << " | " // 1
+//                     << (x <= over(z, !(y))) << " | " // 2
+//                     << nand(!(x), z) << " | " // 3                                 
+//                     << nor(y, nand(!(x), z)) << " | " // 4
+//                     << ((x <= over(z, !(y))) == nor(y, nand(!(x), z))) << " | " << endl; // 5
+//             }
+//         }
+//     }
+
+// }
+
+// int main()
+// {
+//     int a = 1;
+//     int b = 1;
+//     print(a, b);
+// }
+
+
+//№2
+
+// #include <iostream>
+// using namespace std;
+
+// // Антиимпликация: x и не y (x ↛ y)
+// bool antiImpl(bool x, bool y) {
+//     return x && !y;
+// }
+
+// // Антирепликация: не x и y (обратная антиимпликация, y ↛ x)
+// bool antiRepl(bool x, bool y) {
+//     return !x && y;
+// }
+
+// // Штрих Шефера | (NAND): не (x и y)
+// bool nand(bool x, bool y) {
+//     return !(x && y);
+// }
+
+// // Стрелка Пирса ↓ (NOR): не (x или y)
+// bool nor(bool x, bool y) {
+//     return !(x || y);
+// }
+
+// int over(int a, int b) {
+//     if ((a + b) < 2)  {
+//         return a + b;
+//     } else {
+//         return 0;
+//     }
+// }
+
+// void print(int a, int b){
+//     a = 3;
+//     cout << "| x1 | x2 | x3 | 1 | 2 | 3 | 4 |" << endl;
+//     cout << "|:-:|:-:|:-:|:-:|:-:|:-:|:-:|" << endl;
+//     for (int x = 0; x < 2; ++x) {
+//         for (int y = 0; y < 2; ++y) {
+//             for (int z = 0; z < 2; ++z) {
+//                 cout << "| " << x << " | " << y << " | " << z << " | "
+//                     << over(x, y) << " | " // 1
+//                     << antiImpl(x, z) << " | " // 2
+//                     << over(y, antiImpl(x, z)) << " | " // 3                                 
+//                     << nand(over(x, y), over(y, antiImpl(x, z))) << " | " << endl; // 4
+//             }
+//         }
+//     }
+
+// }
+
+// int main()
+// {
+//     int a = 1;
+//     int b = 1;
+//     print(a, b);
+// }
+
+
+// //№3
+// #include <iostream>
+// using namespace std;
+
+// // Антиимпликация: x и не y (x ↛ y)
+// bool antiImpl(bool x, bool y) {
+//     return x && !y;
+// }
+
+// // Антирепликация: не x и y (обратная антиимпликация, y ↛ x)
+// bool antiRepl(bool x, bool y) {
+//     return !x && y;
+// }
+
+// // Штрих Шефера | (NAND): не (x и y)
+// bool nand(bool x, bool y) {
+//     return !(x && y);
+// }
+
+// // Стрелка Пирса ↓ (NOR): не (x или y)
+// bool nor(bool x, bool y) {
+//     return !(x || y);
+// }
+
+// int over(int a, int b) {
+//     if ((a + b) < 2)  {
+//         return a + b;
+//     } else {
+//         return 0;
+//     }
+// }
+
+// void print(int a, int b){
+//     a = 3;
+//     cout << "| x1 | x2 | x3 | 1 | 2 | 3 | 4 |" << endl;
+//     cout << "|:-:|:-:|:-:|:-:|:-:|:-:|:-:|" << endl;
+//     for (int x = 0; x < 2; ++x) {
+//         for (int y = 0; y < 2; ++y) {
+//             for (int z = 0; z < 2; ++z) {
+//                 cout << "| " << x << " | " << y << " | " << z << " | "
+//                     << (z == x) << " | " // 1
+//                     << (y <= (z == x)) << " | " // 2
+//                     << ((y <= (z == x)) && x) << " | " // 3                                 
+//                     << !((y <= (z == x)) && x) << " | " << endl; // 4
+//             }
+//         }
+//     }
+
+// }
+
+// int main()
+// {
+//     int a = 1;
+//     int b = 1;
+//     print(a, b);
+// }
+
+
+#include <iostream>
+using namespace std;
+
+// Антиимпликация: x и не y (x ↛ y)
+bool antiImpl(bool x, bool y) {
+    return x && !y;
+}
+
+// Антирепликация: не x и y (обратная антиимпликация, y ↛ x)
+bool antiRepl(bool x, bool y) {
+    return !x && y;
+}
+
+// Штрих Шефера | (NAND): не (x и y)
+bool nand(bool x, bool y) {
+    return !(x && y);
+}
+
+// Стрелка Пирса ↓ (NOR): не (x или y)
+bool nor(bool x, bool y) {
+    return !(x || y);
+}
+
+int over(int a, int b) {
+    if ((a + b) < 2)  {
+        return a + b;
+    } else {
+        return 0;
+    }
+}
+
+void print(int a, int b){
+    a = 3;
+    cout << "| x1 | x2 | x3 | 1 | 2 | 3 | 4 |" << endl;
+    cout << "|:-:|:-:|:-:|:-:|:-:|:-:|:-:|" << endl;
+    for (int x = 0; x < 2; ++x) {
+        for (int y = 0; y < 2; ++y) {
+            for (int z = 0; z < 2; ++z) {
+                cout << "| " << x << " | " << y << " | " << z << " | "
+                    << (x || !(y) || z) << " | " // 1
+                    << (x || !(y) || !(z)) << " | " // 2
+                    << (!(x) || y || z) << " | " // 3                                 
+                    << ((x || !(y) || z) && (x || !(y) || !(z)) && (!(x) || y || z)) << " | " << endl; // 4
+            }
+        }
+    }
+
+}
+
+int main()
+{
+    int a = 1;
+    int b = 1;
+    print(a, b);
+}
+
