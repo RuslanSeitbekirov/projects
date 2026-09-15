e8dd5b87cf47fbe9dd708385ef04baee9957416e
Melvor-PC <ruslanseitbekirov6632@gmail.com>
2026-09-15 15:47:44 +0300

    dsfs

--- C#/OOP/PR1/obj/Debug/net10.0/PR1.AssemblyInfo.cs
+++ C#/OOP/PR1/obj/Debug/net10.0/PR1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PR1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad52cec4d01e9423ea5f2a7e632ff29d377d9be3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdeb2665a28dcd95d53c0ec5840410671319f7b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PR1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PR1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
