fb16be161fa29c30b3fe0cf923edd8d746f3db95
Melvor-PC <ruslanseitbekirov6632@gmail.com>
2026-09-16 22:40:31 +0300

    вап

--- C#/pr_1/obj/Debug/net10.0/pr_1.AssemblyInfo.cs
+++ C#/pr_1/obj/Debug/net10.0/pr_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pr_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25cd54b3c0f8b32b7a19c15f8a101d284cd60eb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+023b8cdde9c33771c7c5fa283908ad79922fdc5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("pr_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pr_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
