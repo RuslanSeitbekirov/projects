8092412008e734d3e998668bbb920b8db6f81740
Melvor-PC <ruslanseitbekirov6632@gmail.com>
2026-09-21 22:20:18 +0300

    ZD4 comp

--- C#/PracticOOP/PR1/XMLTest/MainWindow.xaml.cs
+++ C#/PracticOOP/PR1/XMLTest/MainWindow.xaml.cs
@@ -4,31 +4,59 @@ using System.Windows.Media;
 
 namespace XMLTest;
 
+/// <summary>
+/// Логика взвимодействия MainWindow.xmal.cs и MainWindow.xmal 
+/// </summary>
+
 public partial class MainWindow : Window
 {
+    /// <summary>
+    /// Обьект класса Random для генерации псевдо случайных чисел
+    /// </summary>
     public Random random = new Random();
 
     public MainWindow()
     {
+    /// <summary>
+    /// Инициализация XAML
+    /// </summary>
         InitializeComponent();
     }
 
+    
+        /// <summary>
+        /// Обработка нажатия кнопки смены Размера
+        /// </summary>
+        /// <param name="sender"></parram>
+        /// <param name="e"></parram>
     private void RandomFormButton_Click(object sender, RoutedEventArgs e)
     {
+        /// <summary>
+        /// для ширины и высоты Label генерируются два разных числа
+        /// </summary>
         int number = Random.Shared.Next(50, 200);
         int number2 = Random.Shared.Next(50, 350);
         Cube.Height = number;
         Cube.Width = number2;
     }
 
+        /// <summary>
+        /// Обработка нажатия кнопки смены цвета
+        /// </summary>
+        /// <param name="sender"></parram>
+        /// <param name="e"></parram>
     private void RandomColorButton_Click(object sender, RoutedEventArgs e)
     {
-
+        /// <summary>
+        /// Смена цвета текста на случайный в палитре RGB
+        /// </summary>
         Cube.Foreground = new SolidColorBrush(Color.FromRgb(
             (byte)random.Next(0, 255),
             (byte)random.Next(0, 255),
             (byte)random.Next(0, 255)));
-
+        /// <summary>
+        /// Смена цвета "задней стенки" на случайный в палитре RGB
+        /// </summary>
         Cube.Background = new SolidColorBrush(Color.FromRgb(
             (byte)random.Next(0, 255),
             (byte)random.Next(0, 255),
